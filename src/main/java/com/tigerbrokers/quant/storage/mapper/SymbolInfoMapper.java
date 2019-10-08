563e331236f7054b05c85af0993b75305057c2b2
windli <wind.li@yeahmobi.com>
2019-10-08 19:48:48 +0800

    优化

--- src/main/java/com/tigerbrokers/quant/storage/mapper/SymbolInfoMapper.java
+++ src/main/java/com/tigerbrokers/quant/storage/mapper/SymbolInfoMapper.java
@@ -16,7 +16,9 @@ import org.apache.ibatis.annotations.Update;
 public interface SymbolInfoMapper {
 
 
-    @Insert({"<script> insert into t_symbol_info(f_contract_id,f_identifier,f_symbol,f_sec_type," +
+    @Insert({"<script> " +
+            "insert into t_symbol_info" +
+            "(" +
             "<if test='expiry != null' >f_expiry,</if>" +
             "<if test='contractMonth != null' >f_contract_month,</if>" +
             "<if test='strike != null' >f_strike,</if>" +
@@ -27,8 +29,10 @@ public interface SymbolInfoMapper {
             "<if test='longInitialMargin != null' >f_long_initial_margin,</if>" +
             "<if test='shortable != null' >f_shortable,</if>" +
             "<if test='longMaintenanceMargin != null' >f_long_maintenance_margin,</if>" +
-            "f_exchange,f_market,f_primary_exchange,f_currency,f_local_symbol,f_name,f_tiger_status,f_min_tick,f_trade,f_continuous,f_trading_class) value " +
-            "(#{contractId},#{identifier},#{symbol},#{secType}," +
+            "f_contract_id,f_identifier,f_symbol,f_sec_type,f_exchange,f_market,f_primary_exchange,f_currency,f_local_symbol,f_name,f_tiger_status,f_min_tick,f_trade,f_continuous,f_trading_class" +
+            ")" +
+            " value " +
+            "(" +
             "<if test='expiry != null' >#{expiry,jdbcType=VARCHAR},</if> " +
             "<if test='contractMonth != null' >#{contractMonth,jdbcType=VARCHAR},</if> " +
             "<if test='strike != null' >#{strike,jdbcType=DOUBLE},</if> " +
@@ -39,7 +43,9 @@ public interface SymbolInfoMapper {
             "<if test='longInitialMargin != null' >#{longInitialMargin,jdbcType=DOUBLE},</if> " +
             "<if test='shortable != null' >#{shortable,jdbcType=INTEGER},</if>" +
             "<if test='longMaintenanceMargin != null' >#{longMaintenanceMargin,jdbcType=DOUBLE},</if>" +
-            "#{exchange},#{market},#{primaryExchange},#{currency},#{localSymbol},#{name},#{status},#{minTick},#{trade},#{continuous},#{tradingClass}) </script>"})
+            "#{contractId},#{identifier},#{symbol},#{secType},#{exchange},#{market},#{primaryExchange},#{currency},#{localSymbol},#{name},#{status},#{minTick},#{trade},#{continuous},#{tradingClass}" +
+            ")" +
+            "</script>"})
     Integer saveSymbol(ContractItem item);
 
     @Update("insert into bar(symbol,open,high,low,close,volume,duration,amount,create_time) value (#{symbol},#{open},#{high},#{low},#{close},#{volume},#{duration},#{amount},now())")
